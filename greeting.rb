8dd4271351577f34a7d54ec3e66afdaa531e4c79
cedric e goddard <8ceg6@comcast.net>
2020-11-29 02:15:45 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,10 @@
+def greeting
+  puts "Hello World"
+end
+
+
+def greeting_programmer(name, language)
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
+
+greeting_programmer ("maria", "ruby")
